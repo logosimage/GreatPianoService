aa24101a7a506396d3bbfd3bd7a2364113b2e58f
Larry Davis <davispcsolutions@gmail.com>
2017-07-18 21:39:45 -0700

    added piano class to admin page

--- GreatPSproj/GreatPSapp/models.py
+++ GreatPSproj/GreatPSapp/models.py
@@ -4,7 +4,17 @@ from accountapp.models import User
 
 
 class Piano(models.Model):
+    MODEL_CHOICES = (
+
+         ('grand','Grand'),
+         ('upright', 'Upright'),
+         ('studio','Studio'),
+         ('spinet','Spinet')
+    )
+
     make = models.CharField(max_length=30)
-    mod = models.CharField(max_length=30)
-    style = models.CharField(max_length=30)
-    serial_num = models.IntegerField()
+    mod = models.CharField(max_length=30, choices=MODEL_CHOICES)
+    serial_num = models.CharField(max_length=30)
+    size = models.CharField(max_length=30)
+    client = models.ForeignKey(User)
+    last_serviced = models.DateField(null=True, blank=True)
