82aee2a6328091799a6c4ff2b7a5b75ec5293774
Larry Davis <davispcsolutions@gmail.com>
2017-07-20 21:28:48 -0700

    added Service_Request class to models

--- GreatPSproj/GreatPSapp/admin.py
+++ GreatPSproj/GreatPSapp/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Piano
+from .models import Piano, Service_Record
 # Register your models here.
 admin.site.register(Piano)
+admin.site.register(Service_Record)
--- GreatPSproj/GreatPSapp/models.py
+++ GreatPSproj/GreatPSapp/models.py
@@ -17,6 +17,7 @@ class Piano(models.Model):
 
         ('tune', 'Tune'),
         ('repair', 'Repair'),
+        ('key services', 'Key Services'),
         ('regulation', 'Regulation'),
         ('voicing', 'Voicing'),
         ('cleaning', 'Cleaning'),
@@ -35,6 +36,24 @@ class Piano(models.Model):
     client = models.ForeignKey(User)
     last_serviced = models.DateField(null=True, blank=True)
     service_request = models.CharField(max_length=30, choices=REQUEST_CHOICES, null=True, blank=True)
+
+    def __str__(self):
+        message = '{}\'s {} #{}'.format(self.client.username, self.make, self.client.customerID)
+        return message
+
+
+class Service_Record(models.Model):
+    piano = models.ForeignKey(Piano)
+    # last_serviced = models.DateField()
+    date = models.DateTimeField()
+    service_performed = models.CharField(max_length=255)
+    service_hours = models.CharField(max_length=10)
+    service_count = models.CharField(max_length=10)
+    service_cost = models.CharField(max_length=10)
+    service_notify = models.CharField(max_length=10)
+
+
+
 
 
 
